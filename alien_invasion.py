66249814ef08e922d965d8843ccd58a1f423938a
Deepak Somashekhara <deepak.somashekhara@gmail.com>
2025-08-09 22:45:02 -0700

    feat: Moves the ship based on left/right arrow keys

--- alien_invasion.py
+++ alien_invasion.py
@@ -15,12 +15,15 @@ class AlienInvasion:
         self._create_window()
         self.ship = Ship(self)
 
+
     def run(self):
         """Start the main executor for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._render_screen()
             self.clock.tick(self.settings.framerate)
+
 
     def _create_window(self):
         """Create the game window."""
@@ -30,17 +33,42 @@ class AlienInvasion:
             self.settings.screen_height
         ))
 
+
     def _check_events(self):
         """Respond to keypresses and mouse events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._handle_keydown(event)
+            elif event.type == pygame.KEYUP:
+                self._handle_keyup(event)
+
 
     def _render_screen(self):
         """Render the screen."""
         self.screen.fill(self.settings.bg_color)
         self.ship.draw()
         pygame.display.flip()
+
+
+    def _handle_keydown(self, event):
+        """Handle keydown events."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.start_moving_right()
+        elif event.key == pygame.K_LEFT:
+            self.ship.start_moving_left()
+        elif event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+            sys.exit()
+
+
+    def _handle_keyup(self, event):
+        """Handle keyup events."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.stop_moving_right()
+        elif event.key == pygame.K_LEFT:
+            self.ship.stop_moving_left()
+
 
 if __name__ == "__main__":
     game = AlienInvasion()
--- settings.py
+++ settings.py
@@ -8,3 +8,5 @@ class Settings:
         self.screen_height = 720
         self.bg_color = (135, 206, 235)
         self.framerate = 60
+        self.speed = 2.5
+        self.step_size = 3
