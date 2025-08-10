f3711ac46071567b148e7774e228ccf39871f693
Deepak Somashekhara <deepak.somashekhara@gmail.com>
2025-08-09 22:45:44 -0700

    feat: Moves the ship based on left/right arrow keys (#3)

--- ship.py
+++ ship.py
@@ -7,17 +7,58 @@ class Ship:
         """Init the ship and set its starting position."""
         self.screen = game.screen
         self.screen_rect = game.screen.get_rect()
+        self.settings = game.settings
 
         # load the ship image
-        self.image = pygame.image.load('images/ship_sky_blue.bmp')
-        self.rect = self.image.get_rect()
+        self.image  = pygame.image.load('images/ship_sky_blue.bmp')
+        self.rect   = self.image.get_rect()
+        self.width  = self.rect.width
+        self.height = self.rect.height
 
         # Place the ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.x = float(self.rect.x)
+
+        self.moving_right = False
+        self.moving_left  = False
+
+
     def draw(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+
+    def update(self):
+        """Update the ship's position."""
+        if self.moving_right:
+            self.x += self.settings.speed * self.settings.step_size
+            self.x  = min(self.x, self.screen_rect.right - self.width)
+        if self.moving_left:
+            self.x -= self.settings.speed * self.settings.step_size
+            self.x  = max(self.x, 0)
+
+        self.rect.x = self.x
+
+
+    def start_moving_right(self):
+        """Ship is moving right."""
+        self.moving_right = True
+
+
+    def start_moving_left(self):
+        """Ship is moving left."""
+        self.moving_left = True
+
+
+    def stop_moving_right(self):
+        """Ship stopped moving right."""
+        self.moving_right = False
+
+
+    def stop_moving_left(self):
+        """Ship stopped moving left."""
+        self.moving_left = False
 
 
 
